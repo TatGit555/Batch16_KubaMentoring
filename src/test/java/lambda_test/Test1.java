e969da79de64b520ac4a49a7932fec186b287e93
Tatiana <tat.r1812@gmail.com>
2023-05-27 14:47:50 -0500

    test2 added

--- src/test/java/lambda_test/Test1.java
+++ src/test/java/lambda_test/Test1.java
@@ -23,41 +23,48 @@ public class Test1 {
         WebDriver driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
-
-        //create a shortcut
-
-        driver.findElement(By.xpath("//a[contains(text(), 'Input Form Submit')"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //create a shortcut
         driver.get("https://www.lambdatest.com/selenium-playground");
+        WebElement form = driver.findElement(By.xpath("//a[contains(text(),'Input Form Submit')]"));
+        Thread.sleep(1000);
+        form.click();
 
         WebElement user = driver.findElement(By.cssSelector("#name"));
+        Thread.sleep(1000);
         user.sendKeys("Sobaka");
-        WebElement email = driver.findElement(By.cssSelector("#email"));
+        WebElement email = driver.findElement(By.cssSelector("#inputEmail4"));
+        Thread.sleep(1000);
         email.sendKeys("123@gmail.com");
         WebElement password = driver.findElement(By.cssSelector("#inputPassword4"));
+        Thread.sleep(1000);
         password.sendKeys("password1");
         WebElement company = driver.findElement(By.cssSelector("#company"));
+        Thread.sleep(1000);
         company.sendKeys("LLC Soba");
         WebElement website = driver.findElement(By.cssSelector("#websitename"));
+        Thread.sleep(1000);
         website.sendKeys("www.sobaka.com");
 
-        WebElement country = driver.findElement(By.xpath("//select[@name='choose option']) "));
-        country.sendKeys(Keys.ARROW_DOWN);
-        country.sendKeys("United States");
-        country.click();
-
+        WebElement city = driver.findElement(By.cssSelector("#inputCity"));
+        Thread.sleep(1000);
+        city.sendKeys("Chicago");
         WebElement address = driver.findElement(By.cssSelector("#inputAddress1"));
-        address.sendKeys("Chicago");
+        Thread.sleep(1000);
+        address.sendKeys("234 Main Str");
         WebElement address2 = driver.findElement(By.cssSelector("#inputAddress2"));
-        address2.sendKeys("234 Main Str");
+        Thread.sleep(1000);
+        address2.sendKeys("apt.4");
         WebElement state = driver.findElement(By.cssSelector("#inputState"));
-        state.sendKeys("ILLINOIS");
         Thread.sleep(1000);
-        WebElement zip = driver.findElement(By.cssSelector("#"));
+        state.sendKeys("Illinois");
+        WebElement zip = driver.findElement(By.cssSelector("#inputZip"));
+        Thread.sleep(1000);
         zip.sendKeys("60606");
-        Thread.sleep(1000);
-        WebElement submit = driver.findElement(By.xpath("//button[@type='Submit']"));
-        Thread.sleep(1000);
+
+        WebElement submit = driver.findElement(By.xpath("//button[@type='submit']"));
+        Thread.sleep(3000);
+        submit.click();
 
         List<WebElement> allmessages = driver.findElements(By.xpath("//p"));
 
@@ -66,11 +73,9 @@ public class Test1 {
                 Assert.assertTrue(allmessages.get(i).isDisplayed());
             Thread.sleep(1000);
         }
-
+        driver.quit();
 
 
 
     }
-
-
 }
---
+++ src/test/java/lambda_test/Test2.java
@@ -0,0 +1,81 @@
+package lambda_test;
+
+import Utils.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.TreeMap;
+
+public class Test2<Map> {
+    @Test
+    public void TC_02() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.lambdatest.com/selenium-playground");
+
+        WebElement pagination = driver.findElement(By.xpath("//a[contains(text(),'Pagination')]"));//a[contains[.,'Pagination'
+        Thread.sleep(1000);
+        pagination.click();
+
+        WebElement dropdown = driver.findElement(By.cssSelector("#maxRows"));
+        Select select = new Select(dropdown);
+        select.selectByValue("5000");
+
+        List<WebElement> allNames = driver.findElements(By.xpath("//tr//td[2]"));
+        List<WebElement> allEmails = driver.findElements(By.xpath("//tr//td[3]"));
+
+        TreeMap<String, String> map = new TreeMap<>();
+        for (int i = 0; i < allNames.size(); i++) {
+            map.put(allNames.get(i).getText(), allEmails.get(i).getText());
+        }
+        System.out.println(map);
+    }
+
+    @Test
+    public void TC_03() throws InterruptedException {
+
+        //find phone numbers
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.lambdatest.com/selenium-playground");
+
+        WebElement pagination = driver.findElement(By.xpath("//a[contains(text(),'Pagination')]"));//a[contains[.,'Pagination'
+        Thread.sleep(1000);
+        pagination.click();
+
+        WebElement dropdown = driver.findElement(By.cssSelector("#maxRows"));
+        Select select = new Select(dropdown);
+        select.selectByValue("5000");
+
+        List<WebElement> allNames = driver.findElements(By.xpath("//tr//td[2]"));
+        List<WebElement> allphones = driver.findElements(By.xpath("//tr//td[4]"));
+
+        TreeMap<String, Long> map = new TreeMap<>();
+        for (int i = 0; i < allNames.size(); i++) {
+            map.put(BrowserUtils.getText(allNames.get(i)), Long.parseLong(BrowserUtils.getText(allphones.get(i)).replace("-","")));
+        }
+        System.out.println(map);
+    }
+
+
+
+    }
+
+
